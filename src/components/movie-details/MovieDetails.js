b240c225341eed8ac2dd899554a5b0d8de7477cf
anthony gorode <anthony.gorode@gmail.com>
2019-08-19 12:06:56 +1100

    Commit 4 : installation de sass sur le projet (npm install node-sass), ajout d'un module sass => MovieElement.module.scss,refonte du css du component MovieElement en important le module sass, rajout de 2 films dans le fichier data.js)

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 export default class MovieDetails extends Component {
     render(){
-        return <div className="w-25 border p-4 d-flex flex-column">
+        return <div className="w-25 bg-light p-4 d-flex flex-column">
             <h5 className="text-center">{this.props.movie.title}</h5>
             <div>
-                <img alt={`illustration du film : ${this.props.movie.title}`} className="d-block mx-auto" src={this.props.movie.img} />
+                <img width="150" height="200" alt={`illustration du film : ${this.props.movie.title}`} className="d-block mx-auto" src={this.props.movie.img} />
             </div>
             <hr className="w-100" />
             <span className="text-secondary">{this.props.movie.details}</span>
--- src/data.js
+++ src/data.js
@@ -19,5 +19,15 @@ export default [
         img: 'https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY209_CR0,0,140,209_AL_.jpg',
         details: 'R | 155 min | Action, Adventure, Drama',
         description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.'
+    }, {
+        title: 'Avengers : Endgame',
+        img: 'http://t3.gstatic.com/images?q=tbn:ANd9GcSbIzUFFVjbCs3bNx3px0sdT38SoU5hrG3mhV5hrUsieuwz62tc',
+        details: 'R | 181 min | Action, Science fiction',
+        description: 'The Titan Thanos, having succeeded in appropriating the six Infinity Stones and reuniting them on the Golden Gauntlet, was able to achieve its objective of pulverizing half of the population of the Universe. Five years later, Scott Lang, aka Ant-Man, manages to escape the subatomic dimension where he was stuck. He offers the Avengers a solution to bring back to life all the missing beings, including their allies and teammates: recover the Stones of Infinity in the past.'
+    }, {
+        title: 'Avatar',
+        img: 'http://images.affiches-et-posters.com//albums/3/47831/medium/affiche-film-avatar.jpg',
+        details: 'R | 162 min | fantasy, Science fiction',
+        description: 'Despite his paralysis, Jake Sully, a former Navy immobilized in a wheelchair, remained a fighter in the depths of his being. He is recruited to go light years from Earth, on Pandora, where powerful industrial groups exploit a rare mineral to solve the energy crisis on Earth.'
     }
 ]
