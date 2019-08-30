1e8d089c56c5526d2386c0ad923715d7c55e44fe
anthony gorode <anthony.gorode@gmail.com>
2019-08-31 09:36:51 +1100

    Commit 16 : mise en place des reducers et des selectors pour les movies

--- src/store/movies/movies.reducers.js
+++ src/store/movies/movies.reducers.js
@@ -1,15 +1,41 @@
+import * as actions from './movies.actions';
 
-// Structure du state
-// {
-//     movies: {
-//         data: [],
-//         selectedMovie: 0,
-//         isLoading: false,
-//         error: null
-//     },
-//     favoris: {
-//         data: [],
-//         isLoading: false,
-//         error: null
-//     }
-// }
+export default (state = {
+    data: [],
+    isLoading: false,
+    error: null,
+    selectedMovie: 0
+}, action) => {
+    switch(action.type){
+        case actions.REQUEST_MOVIES:
+            return {
+                ...state,
+                isLoading: true
+            }
+        
+        case actions.FETCH_MOVIES_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                error: null,
+                data: [...action.movies]
+            }
+
+        case actions.FETCH_MOVIES_ERROR:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.error
+            }
+
+        case actions.SET_SELECTED_MOVIE:
+            return {
+                ...state,
+                selectedMovie: action.index
+            }
+        
+        default: {
+            return state;
+        }
+    }
+}
---
+++ src/store/reducers.js
@@ -0,0 +1,19 @@
+import movies from './movies/movies.reducers';
+// Structure du state
+// {
+//     movies: {
+//         data: [],
+//         selectedMovie: 0,
+//         isLoading: false,
+//         error: null
+//     },
+//     favoris: {
+//         data: [],
+//         isLoading: false,
+//         error: null
+//     }
+// }
+
+export default {
+    movies
+}
