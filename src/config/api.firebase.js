c47d3c78a0da50f37b468d7dbb8d7a0dde12bc78
anthony gorode <anthony.gorode@gmail.com>
2019-08-31 20:11:54 +1100

    Commit 17 : mise en place des actions pour les favoris

--- src/config/api.firebase.js
+++ src/config/api.firebase.js
@@ -4,4 +4,11 @@ const apiFirebase = axios.create({
     baseURL: 'https://allomovie-adb07.firebaseio.com/'
 });
 
-export default apiFirebase;
+export const apiFirebase;
+
+export default {
+    fetchFavoris: () => apiFirebase.get('favoris.json').then(
+        response => response.data ? response.data : []
+    ),
+    saveFavoris: favoris => () => apiFirebase.put('favoris.json', favoris)
+}
